1b85db016f782e446a2e943ea90a7b135eb87cd0
RoyJian <roy881124@icloud.com>
2022-06-09 15:52:05 +0800

    add:replica count

--- controllers/url_controller.js
+++ controllers/url_controller.js
@@ -1,6 +1,6 @@
 const {CreateURL,GetOriginalURL} = require('../models/url_model');
 const getRandomInt = require('../utils/GetRandomInt');
-const {DB_HOST_p1,DB_HOST_p2,DB_HOST_p3,DB_Num} = process.env;
+const {DB_HOST_p1,DB_HOST_p2,DB_HOST_p3,DB_Num,DB_REPLICA_COUNT} = process.env;
 
 const CreateUrl = async (req,res)=>{
     const url = req.body.original_url;
@@ -15,7 +15,7 @@ const CreateUrl = async (req,res)=>{
 const DecodeUrl = async(req,res)=>{
     const hash = req.query.id
     const decode = new Buffer.from(hash,'base64').toString('utf-8');
-    const replicaSelect = getRandomInt(2) + 1;
+    const replicaSelect = getRandomInt(parseInt(DB_REPLICA_COUNT)) + 1;
     const addressArr = decode.split(',');
     const host = `${DB_HOST_p1}${addressArr[0]}-${replicaSelect}${DB_HOST_p2}${DB_HOST_p3}`;
     const index = addressArr[1];
