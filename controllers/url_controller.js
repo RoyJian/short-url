701dbf53a13f2f591ed515ea07c9b80db59164d1
RoyJian <roy881124@icloud.com>
2022-06-09 12:01:29 +0800

    feat:add replica MySQL

--- controllers/url_controller.js
+++ controllers/url_controller.js
@@ -1,10 +1,11 @@
 const {CreateURL,GetOriginalURL} = require('../models/url_model');
-const {DB_HOST_1,DB_HOST_2,DB_Num} = process.env;
+const getRandomInt = require('../utils/GetRandomInt');
+const {DB_HOST_p1,DB_HOST_p2,DB_HOST_p3,DB_Num} = process.env;
 
 const CreateUrl = async (req,res)=>{
     const url = req.body.original_url;
     const id =  await CreateURL(url);
-    let RDSip = DB_Num + '.cub7m7zd2cmg,' + id.toString();
+    let RDSip = `${DB_Num},${id.toString()}`;
     RDSip = new Buffer.from(RDSip,"utf-8");
     RDSip = RDSip.toString('base64');
     res.send({id:id,url:RDSip});
@@ -14,8 +15,10 @@ const CreateUrl = async (req,res)=>{
 const DecodeUrl = async(req,res)=>{
     const hash = req.query.id
     const decode = new Buffer.from(hash,'base64').toString('utf-8');
-    const host = DB_HOST_1 + decode.split(',')[0] + DB_HOST_2;
-    const index = decode.split(',')[1];
+    const replicaSelect = getRandomInt(1) + 1;
+    const addressArr = decode.split(',');
+    const host = `${DB_HOST_p1}${addressArr[0]}-${replicaSelect}${DB_HOST_p2}${DB_HOST_p3}`;
+    const index = addressArr[1];
     const originUrl = await GetOriginalURL(host,index);
     res.send({originUrl,host});
 };
