fd3ddd3f3190b3a5f08c9e822c410029cb1a1940
RoyJian <roy881124@icloud.com>
2022-06-09 13:53:00 +0800

    fix:use execute db fucntion

--- models/url_model.js
+++ models/url_model.js
@@ -7,14 +7,14 @@ const CreateURL = async(original_url)=>{
         id : null,
         original_url:original_url
     }
-    const sqlres = await db.query({sql:sql,values:values});
+    const sqlres = await db.execute({sql:sql,values:values});
     console.log(sqlres[0]['insertId']);
     return (sqlres[0]['insertId']);
 }
 const GetOriginalURL = async(host,index)=>{
     const sql = 'SELECT original_url FROM shorturl where id = ?;'
     const RDSdb = await otherDB(host);
-    const [[res]] = await RDSdb.query(sql,[index]);
+    const [[res]] = await RDSdb.execute(sql,[index]);
     console.log(res.original_url);
     return res.original_url;
 
